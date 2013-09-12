48cc2b6303eea08574b21a5f236917366b5b048d
xianhma <xianhma@ubuntu.(none)>
2013-09-12 12:06:14 -0700

    add methods to spec

--- lib/jruby_whirr/cluster_spec.rb
+++ lib/jruby_whirr/cluster_spec.rb
@@ -15,9 +15,38 @@ module JrubyWhirr
 			@cluster_spec.getServiceName()
 		end
 
+		def get_private_key ()
+			@cluster_spec.getPrivatekey()
+		end
+
+		def set_cluster_user ()
+			@cluster_spec.getClusterUser()
+		end
+
+		def set_provider()
+			@cluster_spec.getProvider()
+		end
+
 		def set_service_name (name)
 			@cluster_spec.setServiceName(name)
 		end
+
+		def set_public_key (public_key)
+			@cluster_spec.setPublicKey(public_key)
+		end
+
+		def set_private_key (private_key)
+			@cluster_spec.setPrivatekey(private_key)
+		end
+
+		def set_cluster_user (user)
+			@cluster_spec.setClusterUser(user)
+		end
+
+		def set_provider(provider)
+			@cluster_spec.setProvider(provider)
+		end
+
 
 		
 	end
